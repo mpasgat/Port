277d0aa7bbf86ea38c22fe31bb675bfc47beb245
mpasgat <157752876+mpasgat@users.noreply.github.com>
2024-07-03 23:22:41 +0300

    Add files via upload

---
+++ src/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<header style="background-color: blueviolet; color: #fff; padding: 10px 0; text-align: center;">
+	<h1>{title}</h1>
+</header>
---
+++ src/Projects.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let projects: { title: string; description: string }[];
+</script>
+
+<section
+	class="projects"
+	style="padding: 20px; background: white; margin-top: 20px; box-shadow: 0 0 10px
+	 rgba(0, 0, 0, 0.1); border-radius: 10px;"
+>
+	<h2>Projects</h2>
+	<ul>
+		{#each projects as { title, description }}
+			<li>
+				<strong>{title}:</strong>
+				{description}
+			</li>
+		{/each}
+	</ul>
+</section>
