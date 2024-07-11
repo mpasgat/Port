ba3b554cae663f836127b9ed50814d214d800033
mpasgat <157752876+mpasgat@users.noreply.github.com>
2024-07-11 20:27:10 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    paths: {
-      base: dev ? '' : '/Port'
-    },
-    prerender: {
-      default: true
-    }
-  }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
