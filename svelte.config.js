d62a1ff4d161d2b1a79d23367a5c549016f8edee
mpasgat <157752876+mpasgat@users.noreply.github.com>
2024-07-29 01:03:13 +0300

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
-
-export default {
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess:vitePreprocess(),
 	kit: {
+
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+
+		}),
+		paths: {
+			base: '/Port'
+		}
 	}
 };
+
+export default config;
