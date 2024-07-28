170674f94aeabd25bcb72ae87dccfe4e082fb238
mpasgat <157752876+mpasgat@users.noreply.github.com>
2024-07-29 01:06:45 +0300

    Delete src directory

--- src/Header.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let title: string;
-</script>
-
-<header style="background-color: blueviolet; color: #fff; padding: 10px 0; text-align: center;">
-	<h1>{title}</h1>
-</header>
